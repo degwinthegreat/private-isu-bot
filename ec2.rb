31bbaee3367670180403ca5bc1526e971df484a6
degwinthegreat <the.colledge.dropout@gmail.com>
2022-06-28 21:43:39 +0900

    Add app2

--- ec2.rb
+++ ec2.rb
@@ -13,14 +13,14 @@ class Ec2
 
   attr_reader :ec2
 
-  def instance_status(type)
+  def instance_status(service_name)
     status = []
-    instance_id = type == :app ? app_instance_id : bench_instance_id
+    instance_id = instance_id_find_by(service_name)
     res = ec2.describe_instance_status(instance_ids: [instance_id])
     if res.instance_statuses[0]
       if res.instance_statuses[0][:instance_status].empty?
         status << 'stopped'
-        message = "#{type} instance has stopped"
+        message = "#{service_name} instance has stopped"
       else
         res.instance_statuses.map do |s|
           sys_status = s.respond_to?(:system_status) ? s.system_status.details[0].status : nil
@@ -28,83 +28,95 @@ class Ec2
           status << sys_status << ins_status
         end
         message = if status.include?('passed')
-                    "#{type} instance is running"
+                    "#{service_name} instance is running"
                   elsif status.include?('initializing')
-                    "#{type} instance is starting"
+                    "#{service_name} instance is starting"
                   else
-                    "#{type} instance is unknown status"
+                    "#{service_name} instance is unknown status"
                   end
       end
     else
       status << 'terminated'
-      message = "#{type} instance has already terminated"
+      message = "#{service_name} instance has already terminated"
     end
     message
   end
 
-  def instance_start(type)
-    instance_id = type == :app ? app_instance_id : bench_instance_id
+  def instance_start(service_name)
+    instance_id = instance_id_find_by(service_name)
     res = ec2.describe_instance_status(instance_ids: [instance_id])
 
     message = if res.instance_statuses.count.positive?
                 state = res.instance_statuses[0].instance_state.name
                 case state
                 when 'pending'
-                  return "Error starting instance: #{type} instance is pending. Try again later."
+                  return "Error starting instance: #{service_name} instance is pending. Try again later."
                 when 'running'
-                  return "#{type} instance is already running."
+                  return "#{service_name} instance is already running."
                 when 'terminated'
                   return 'Error starting instance: ' \
-                              "#{type} the instance is terminated, so you cannot start it."
+                              "#{service_name} the instance is terminated, so you cannot start it."
                 end
               end
     return message if message
 
     ec2.start_instances(instance_ids: [instance_id])
     ec2.wait_until(:instance_running, instance_ids: [instance_id])
-    "#{type} Instance started."
+    "#{service_name} Instance started."
   rescue StandardError => e
     "Error starting instance: #{e.message}"
   end
 
-  def instance_stop(type)
-    instance_id = type == :app ? app_instance_id : bench_instance_id
+  def instance_stop(service_name)
+    instance_id = instance_id_find_by(service_name)
     res = ec2.describe_instance_status(instance_ids: [instance_id])
 
     message = if res.instance_statuses.count.positive?
                 state = res.instance_statuses[0].instance_state.name
                 case state
                 when 'stopping'
-                  return "#{type} instance is already stopping."
+                  return "#{service_name} instance is already stopping."
                 when 'stopped'
-                  return "#{type} instance is already stopped."
+                  return "#{service_name} instance is already stopped."
                 when 'terminated'
                   return 'Error stopping instance: ' \
-                          "#{type} instance is terminated, so you cannot stop it."
+                          "#{service_name} instance is terminated, so you cannot stop it."
                 end
               end
     return message if message
 
     ec2.stop_instances(instance_ids: [instance_id])
     ec2.wait_until(:instance_stopped, instance_ids: [instance_id])
-    "#{type} Instance stopped."
+    "#{service_name} Instance stopped."
   rescue StandardError => e
     "Error stopping instance: #{e.message}"
   end
 
-  def instance_public_ip(type)
-    instance_id = type == :app ? app_instance_id : bench_instance_id
+  def instance_public_ip(service_name)
+    instance_id = instance_id_find_by(service_name)
     res = ec2.describe_instances(instance_ids: [instance_id])
     ip = res.reservations[0]&.instances[0]&.public_ip_address
-    return "#{type} instance has already terminated" unless ip
+    return "#{service_name} instance has already terminated" unless ip
 
-    "#{type} public ip address: #{ip}"
+    "#{service_name} public ip address: #{ip}"
   end
 
   private
 
+  def instance_id_find_by(service_name)
+    case service_name
+    when :app then app_instance_id
+    when :app2 then app2_instance_id
+    when :bench then bench_instance_id
+    end
+  end
+
   def app_instance_id
     ENV['APP_INSTANCE_ID']
+  end
+
+  def app2_instance_id
+    ENV['APP2_INSTANCE_ID']
   end
 
   def bench_instance_id
--- main.rb
+++ main.rb
@@ -20,11 +20,17 @@ client.on :message do |message|
   when '/status_app', '/app_status'
     status_message = ec2.instance_status(:app)
     message.channel.post(status_message)
+  when '/status_app2', '/app_status2'
+    status_message = ec2.instance_status(:app2)
+    message.channel.post(status_message)
   when '/status_bench', '/bench_status'
     status_message = ec2.instance_status(:bench)
     message.channel.post(status_message)
   when '/start_app', '/app_start'
     log = ec2.instance_start(:app)
+    message.channel.post(log)
+  when '/start_app2', '/app_start2'
+    log = ec2.instance_start(:app2)
     message.channel.post(log)
   when '/start_bench', '/bench_start'
     log = ec2.instance_start(:bench)
@@ -32,11 +38,17 @@ client.on :message do |message|
   when '/stop_app', 'app_stop'
     log = ec2.instance_stop(:app)
     message.channel.post(log)
+  when '/stop_app2', 'app_stop2'
+    log = ec2.instance_stop(:app2)
+    message.channel.post(log)
   when '/stop_bench', '/bench_stop'
     log = ec2.instance_stop(:bench)
     message.channel.post(log)
   when '/ip_app', '/app_ip'
     res = ec2.instance_public_ip(:app)
+    message.channel.post(res)
+  when '/ip_app2', '/app_ip2'
+    res = ec2.instance_public_ip(:app2)
     message.channel.post(res)
   when '/ip_bench', '/bench_ip'
     res = ec2.instance_public_ip(:bench)
